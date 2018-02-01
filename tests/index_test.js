d0c3eee51422046c0953397450a809d7f0dfa4c2
Frank Holder <frank.holder@fitx.de>
2018-02-01 16:56:07 +0100

    add input test

--- tests/index_test.js
+++ tests/index_test.js
@@ -3,8 +3,7 @@ Feature('list overview');
 Scenario('home test scenario', (I) => {
   I.amOnPage('/');
   I.seeInCurrentUrl('/');
-  //I.fillField('#input-name', 'hello world');
-
-I.see('Hello World');
+  I.fillField('#input-name', 'hello world');
+  I.see('Hello World');
   I.click('Submit');
 });
